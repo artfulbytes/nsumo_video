dfa2e83904dfe5a1471dc6a3eccfa2ca4c9522fc
Niklas Nilsson <niklas@artfulbytes.com>
2022-12-03 16:20:52 +0100

    feat(drivers): implement IO handling
    The pinmapping for the Launchpad and the actual PCB (Nsumo) are
    represented with an enum (io_e). Create a second enum (io_generic_e),
    define macros, and static register arrays (e.g. port_dir_regs) to map
    the enum values to the corresponding registers in a space-efficient way.
    Implement the individual functions to select function, set resistor,
    direction, and output. The result is an abstraction that's much nicer
    than directly accessing the crude register variables from TI.
    
    video: 12

---
+++ src/drivers/mcu_init.h
@@ -0,0 +1,9 @@
+#ifndef MCU_INIT_H
+#define MCU_INIT_H
+
+/* Initialization of common microcontroller functionality that affects all peripherals
+ * (e.g. watchdog, clocks, pins) */
+
+void mcu_init(void);
+
+#endif // MCU_INIT_H
