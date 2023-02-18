dc509ce8100da0079298bbf01548b3d1edc4bb09
Niklas Nilsson <niklas@artfulbytes.com>
2023-02-18 14:41:10 +0100

    fix(init): increase clock speed from 1 MHz to 16 MHz
    Increase the clock speed to 16 MHz mostly because I can. The clock speed
    is not the limiting factor in this application, so technically there is
    no reason to run it at 16 MHz, but the power consumption is negligible
    compared to other things, and if I'm going to change it it's better to
    do it now rather than later.
    
    video: 17

--- src/common/defines.h
+++ src/common/defines.h
@@ -6,9 +6,10 @@
 #define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
 #define INTERRUPT_FUNCTION(vector) void __attribute__((interrupt(vector)))
 
-// TODO: Change clock rate from 1 MHz to 16 MHz
 #define CYCLES_1MHZ (1000000u)
-#define ms_TO_CYCLES(ms) ((CYCLES_1MHZ / 1000u) * ms)
+#define CYCLES_16MHZ (16u * CYCLES_1MHZ)
+#define CYCLES_PER_MS (CYCLES_16MHZ / 1000u)
+#define ms_TO_CYCLES(ms) (CYCLES_PER_MS * ms)
 #define BUSY_WAIT_ms(ms) (__delay_cycles(ms_TO_CYCLES(ms)))
 
 #endif // DEFINES_H
